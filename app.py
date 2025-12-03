99a92961236a7cb37fb79771fae6898153c36ba6
Nabilaramadhanis <202210715258@mhs.ubharajaya.ac.id>
2025-12-03 11:29:57 +0700

    app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.write("Aplikasi Machine Learning untuk memprediksi popularitas lagu menggunak
 # Load Data
 @st.cache_data
 def load_data():
-    df = pd.read_csv("spotify_data clean.csv")
+    df = pd.read_csv("spotify_data_clean.csv")
     return df
 
 df = load_data()
@@ -71,4 +71,5 @@ try:
 
 except:
     st.warning("⚠ Latih model dulu sebelum melakukan prediksi!")
+
 
